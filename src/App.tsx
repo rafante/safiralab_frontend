3c35a7324c97dda5421cb66444cb140649ae702c
juscelino <juscelinotrevenzoli@gmail.com>
2019-11-02 15:48:45 -0300

    semantic ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Button, Icon, Container, Grid } from 'semantic-ui-react';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Grid className="p-bot" columns={2}>
+        <Grid.Row  stretched>
+          <Grid.Column width={1} color="brown"></Grid.Column>
+          <Grid.Column stretched color="blue"></Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </Container>
   );
 }
 
