a7ff4b5f3916c53ccbbac4ef3cda3ac32de47276
juscelino <juscelinotrevenzoli@gmail.com>
2019-11-02 16:59:19 -0300

    Criando Tela inicial

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
-import React from 'react';
-import './App.css';
-import { Segment, Icon, Container, Grid } from 'semantic-ui-react';
+import React from "react";
+import "./App.css";
+import { Segment, Icon, Container, Grid } from "semantic-ui-react";
+import MenuLateral from "./components/MenuLateral";
 
 const App: React.FC = () => {
   return (
     <Container fluid>
-      <Grid columns={2}>
-        <Grid.Row stretched>
-
-        </Grid.Row>
-        <Grid.Row stretched>
-          <Grid.Column width={3}></Grid.Column>
-          <Grid.Column width={13}>
-            <Segment content >
-              <Container fluid></Container>
-            </Segment>
+      <Grid padded divided columns={2} style={{ height: "100vh" }}>
+        <Grid.Row style={{ height: "100%" }} stretched>
+          <Grid.Column color="blue" width={3}>
+            <MenuLateral />
+          </Grid.Column>
+          <Grid.Column color="blue" width={13}>
+            <Grid.Row color="green" as={Segment}></Grid.Row>
+            <Grid.Row color="red" as={Segment}></Grid.Row>
           </Grid.Column>
         </Grid.Row>
       </Grid>
     </Container>
   );
-}
+};
 
 export default App;
