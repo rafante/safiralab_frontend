1f9986dd973b2bb55e6a3e5e269316fc6bac360c
rafante <rafante2@gmail.com>
2019-11-02 16:09:25 -0300

    layout basico inicio

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import {Button, Icon, Container, Grid } from 'semantic-ui-react';
+import { Segment, Icon, Container, Grid } from 'semantic-ui-react';
 
 const App: React.FC = () => {
   return (
-    <Container>
-      <Grid className="p-bot" columns={2}>
-        <Grid.Row  stretched>
-          <Grid.Column width={1} color="brown"></Grid.Column>
-          <Grid.Column stretched color="blue"></Grid.Column>
+    <Container fluid>
+      <Grid columns={2}>
+        <Grid.Row stretched>
+
+        </Grid.Row>
+        <Grid.Row stretched>
+          <Grid.Column width={3}></Grid.Column>
+          <Grid.Column width={13}>
+            <Segment content >
+              <Container fluid></Container>
+            </Segment>
+          </Grid.Column>
         </Grid.Row>
       </Grid>
     </Container>
